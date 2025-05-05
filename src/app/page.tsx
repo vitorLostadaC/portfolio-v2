c51cb06dac763faa9ae062ddf4cb94a72897a466
VitorLostadaC <vitorlostada@hotmail.com>
2025-05-04 22:03:34 -0300

    feat: lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import { Button } from '@/components/ui/button'
 export default function Home() {
 	return (
 		<div>
-			<Button>sdf</Button>
+			<Button className=" text-white xl:sm-5  bg-red-500  hover:bg-red-600 sm:w-4 ">
+				sdf
+			</Button>
 		</div>
 	)
 }
