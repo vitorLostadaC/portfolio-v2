44ed01a41e411ac363953661710405a05807db09
VitorLostadaC <vitorlostada@hotmail.com>
2025-07-09 16:17:26 -0300

    feat: add biome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
-  return (
-    <div>
-      <Button>Click me</Button>
-      <p>Opa tudo bem eu me chamo VariantProps</p>
-    </div>
-  );
+	return (
+		<div>
+			<Button>Click me</Button> <p>Opa tudo bem eu me chamo VariantProps</p>{' '}
+		</div>
+	)
 }
